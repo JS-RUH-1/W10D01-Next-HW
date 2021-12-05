b7328cb6b98091c9fed8b9565be613695f61a10f
Turki700 <Turki.Haqawi.ruh.js@tuwaiq.edu.sa>
2021-12-05 17:02:36 +0300

    HW Done

---
+++ next_app/pages/index.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/albums');
+  const data = await res.json();
+
+  return {
+    props: { albums: data }
+  }
+}
+
+const Home = ({ albums }) => {
+  // console.log(ninjas)
+
+  return (
+    <div>
+      <h1>All Albums</h1>
+      {albums.map(album => (
+        <Link href={'/albums/' + album.id} key={album.id}>
+          <a>
+            <h3>{album.title}</h3>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
