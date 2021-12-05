b7f829cfb6507c8ae828fb46f9614c852ae2ae06
Maha Alharbi <m.a.alharbi995@gmail.com>
2021-12-05 11:49:41 +0300

    Done => Header , Footer, 404 page

---
+++ nexthw/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+    <footer>
+        <div className="container">
+            <p className="text-center py-3 text-secondary">
+                Next-HW &copy; 2021</p>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer
---
+++ nexthw/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Next-HW
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About Us
+                </a>
+              </li>
+              {/* <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li> */}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
